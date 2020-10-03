3ca501105b7659aebfb1068d13bc747d557d9b62
Oscar van Eijk <oscar.van.eijk@oveas.com>
2020-10-03 12:39:31 +0200

    Copied a bit too much from my other project (a WP plugin)

--- src/apps/ttadmin/install.php
+++ src/apps/ttadmin/install.php
@@ -8,21 +8,6 @@
 define ('TT__BASE__INSTALL', true);
 error_reporting(E_ALL | E_STRICT);
 
-if (false) {
-	// Enable WP_DEBUG mode
-	define( 'WP_DEBUG', true );
-	
-	// Enable Debug logging to the /wp-content/debug.log file
-	define( 'WP_DEBUG_LOG', false );
-	
-	// Disable display of errors and warnings
-	define( 'WP_DEBUG_DISPLAY', true );
-	@ini_set( 'display_errors', 1 );
-	
-	// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
-	define( 'SCRIPT_DEBUG', false );
-}
-
 // We assume the Terra-Terra library is at the same level and its name is terra-terra
 define ('TT_ROOT', dirname(dirname(dirname(__FILE__))));
 require (TT_ROOT . '/TTinstaller.php');
--- src/index.php
+++ src/index.php
@@ -7,21 +7,6 @@
  */
 error_reporting (E_ALL | E_STRICT);
 
-if (true) {
-	// Enable WP_DEBUG mode
-	define( 'WP_DEBUG', true );
-	
-	// Enable Debug logging to the /wp-content/debug.log file
-	define( 'WP_DEBUG_LOG', false );
-	
-	// Disable display of errors and warnings
-	define( 'WP_DEBUG_DISPLAY', true );
-	@ini_set( 'display_errors', 1 );
-	
-	// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
-	define( 'SCRIPT_DEBUG', false );
-}
-
 define ('TT_ROOT', dirname(__FILE__));
 
 define ('TT_TIMERS_ENABLED', true);
